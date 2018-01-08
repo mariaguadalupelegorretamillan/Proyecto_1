4c96f616130eee3c1359003061578490d53ea93b
William Ochoa <wsteven24g@gmail.com>
2018-01-08 11:12:26 -0500

    Add project files.

---
+++ WebLogin1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebLogin1.Startup))]
+namespace WebLogin1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
